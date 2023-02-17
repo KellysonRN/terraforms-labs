b3e98efecbe3a09f1f26fa20cdf7072ee1e7cccb
Kellyson Santos <kellyson.victor@gmail.com>
2023-02-17 10:22:47 -0300

    Create app service module

--- azure/examples/app-service/main.tf
+++ azure/examples/app-service/main.tf
@@ -1,20 +1,27 @@
-variable "proof_name" {
-  description = "The name od your static website"
-  type        = string
+resource "azurerm_service_plan" "service_plan" {
+  name                = "serviceplan-${var.proof_name}-exp-${var.location}"
+  location            = var.location
+  resource_group_name = var.rg
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-variable "location" {
-  description = "Azure location this azure StorageAccount should reside in"
-  type        = string
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "api-${var.proof_name}-exp-${var.location}"
+  location            = var.location
+  resource_group_name = var.rg
+  service_plan_id     = azurerm_service_plan.service_plan.id
+  https_only          = true
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on           = false
+  }
 }
 
-variable "tags" {
-  description = "Tags to add to resources"
-  type        = map(string)
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                 = azurerm_linux_web_app.webapp.id
+  repo_url               = var.repo
+  branch                 = "main"
+  use_manual_integration = true
+  use_mercurial          = false
 }
-
-variable "rg" {
-  description = "Resources Group"
-  type        = string
-}
-
---
+++ azure/examples/app-service/vars.tf
@@ -0,0 +1,24 @@
+variable "proof_name" {
+  description = "The name od your static website"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location this azure StorageAccount should reside in"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to add to resources"
+  type        = map(string)
+}
+
+variable "rg" {
+  description = "Resources Group"
+  type        = string
+}
+
+variable "repo" {
+  description = "Package from webapi"
+  type        = string
+}
